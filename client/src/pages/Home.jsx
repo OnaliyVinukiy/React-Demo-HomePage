1da4f702bad1701c5e40bee3033528da01584608
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-01-03 15:26:18 +0530

    imported header

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Carousel } from 'flowbite-react';
+import Header from "../components/Header";
+
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+    
+      <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+        <Carousel slideInterval={5000}>
+        <img src="src/images/sale.jpg" alt="..." style={{  width: '100%', height: '100%' }} />
+        <img src="src/images/sale2.jpg" alt="..." style={{ width: '100%', height: '100%' }} />
+        </Carousel>
+      </div>
     </div>
   );
 }
