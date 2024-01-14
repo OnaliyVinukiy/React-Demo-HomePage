31e8fa958c020e0036602bac0cc8ac3eaf197b0b
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-01-14 13:08:57 +0530

    modified header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
           <div className="hidden w-full md:block md:w-auto" id="navbar-multi-level">
             <ul className="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-gray-900 dark:bg-gray-800 dark:border-gray-700">
               <li>
-                <a href="#" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-400 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</a>
+                <a href="/" className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-400 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</a>
               </li>
               <li>
                 <button
