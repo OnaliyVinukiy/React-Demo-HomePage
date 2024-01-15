9c8bdc1f4922cc4bf7441321fe830968b8e583b1
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-01-15 17:18:32 +0530

    changed rounded corners of the slider

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import { Carousel } from 'flowbite-react';
 import Header from "../components/Header";
 
-
 export default function Home() {
   return (
     <div>
-    
       <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
         <Carousel slideInterval={5000}>
-        <img src="src/images/emotion.png" alt="..." style={{  width: '100%', height: '100%' }} />
-        <img src="src/images/emotion.png" alt="..." style={{ width: '100%', height: '100%' }} />
+          <img src="src/images/emotion.png" alt="..." style={{ width: '100%', height: '100%' }} />
+          <img src="src/images/emotion.png" alt="..." style={{ width: '100%', height: '100%' }} />
         </Carousel>
       </div>
     </div>
