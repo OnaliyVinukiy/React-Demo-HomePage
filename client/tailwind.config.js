6576e5af1ddc9c46f714832ebcc33c5b3490d7ee
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-01-24 16:39:22 +0530

    added flowbite js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     'node_modules/flowbite-react/lib/esm/**/*.js',
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
@@ -11,6 +12,7 @@ export default {
   plugins: [
     require('flowbite/plugin'),
   ],
+  
 }
 
   
