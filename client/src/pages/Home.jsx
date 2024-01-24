247c1c9008663df4e33139ad6b0e82a72e9abd8b
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-01-24 16:41:51 +0530

    modified carousel slider

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,40 +4,28 @@ import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div id="default-carousel" className="relative w-full" data-carousel="slide">
-    <div className="relative h-96 overflow-hidden lg md:h-144">
-        <div className="hidden duration-500 ease-in-out" data-carousel-item> {/* Increased duration */}
-          <img src="src/images/emotion.png" alt="..." style={{ width: '100%', height: '100%' }} />
-        </div>
-        <div className="hidden duration-500 ease-in-out" data-carousel-item> {/* Increased duration */}
-          <img src="src/images/safe.jpeg" alt="..." style={{ width: '100%', height: '100%' }} />
-        </div>
-        <div className="hidden duration-500 ease-in-out" data-carousel-item> {/* Increased duration */}
+    <div>
+      <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+        <Carousel slideInterval={5000} theme={{
+         
+         "item": {
+          "base": "absolute top-1/2 left-1/2 block w-full -translate-x-1/2 -translate-y-1/2",
+          "wrapper": {
+            "off": "w-full flex-shrink-0 transform cursor-default snap-center",
+            "on": "w-full flex-shrink-0 transform cursor-grab snap-center"
+          }
+        },
+        "scrollContainer": {
+          "base": "flex h-full snap-mandatory overflow-y-hidden overflow-x-scroll scroll-smooth", // Remove rounded-lg here
+          "snap": "snap-x"
+        }
+          
+        }}>
           <img src="src/images/spandana.jpeg" alt="..." style={{ width: '100%', height: '100%' }} />
-        </div>
+          <img src="src/images/emotion.png" alt="..." style={{ width: '100%', height: '100%' }} />
+          <img src="src/images/safe.jpeg" alt="..." style={{ width: '100%', height: '100%' }} />
+        </Carousel>
       </div>
-      <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-        <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-      </div>
-      <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-            </svg>
-            <span class="sr-only">Previous</span>
-        </span>
-    </button>
-    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-            </svg>
-            <span class="sr-only">Next</span>
-        </span>
-    </button>
-</div>
-
+    </div>
   );
 }
